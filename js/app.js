840aca45a0f74f1a046da806bb3df4a422b22a79
Heydon Pickering <heydon@heydonworks.com>
2018-09-06 11:00:42 +0100

    and files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/* Components */
+import Image from './components/Image.js';
+
+/* Paint worker properties */
+if ('registerProperty' in CSS) {
+  CSS.registerProperty({
+    name: '--border-thin-h',
+    syntax: '<length>',
+    initialValue: getComputedStyle(document.documentElement).getPropertyValue("--border-thin")
+  });
+
+  CSS.registerProperty({
+    name: '--color-dark-h',
+    syntax: '<color>',
+    initialValue: getComputedStyle(document.documentElement).getPropertyValue("--color-dark")
+  });
+}
+
+/* Register paint workers */
+if ('paintWorklet' in CSS) {
+  CSS.paintWorklet.addModule('js/houdini/image-cross.js');
+}
---
+++ js/components/Image.js
@@ -0,0 +1,27 @@
+import ratioHeight from '../utilities/ratioHeight.js';
+
+export default class Image extends HTMLElement {
+  constructor() {
+    super();
+    this.setAttribute('role', 'img');
+    const ratio = this.getAttribute('ratio')
+    this.setAttribute('aria-label', `Image with ${ratio} ratio`);
+
+    const padding = ratioHeight(ratio) + '%';
+
+    const tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <style>
+        :host {
+          padding-bottom: ${padding};
+        }
+      </style>
+      <slot></slot>
+    `;
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(tmpl.content.cloneNode(true));
+  }
+}
+
+customElements.define('i-mage', Image);
