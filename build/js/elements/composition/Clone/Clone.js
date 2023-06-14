7525ee63e07351f8b330c566209ea5f4eda57c22
Heydon <heydon@heydonworks.com>
2023-06-14 12:16:10 +0100

    clone element

---
+++ build/js/elements/composition/Clone/Clone.js
@@ -0,0 +1,37 @@
+import { BaseElement } from '../../Base.js';
+
+/**
+ * @tag c-lone
+ * @attr {string} of - Corresponds to the id of the template element whose content’s should be cloned
+ * @summary Clones the content of a referenced template element as a subtree
+ */
+class CloneElement extends BaseElement {
+	get of() {
+		return this.getAttribute('of');
+	}
+
+	set of(value) {
+		if (value) {
+			this.setAttribute('of', value);
+		} else {
+			this.removeAttribute('of');
+		}
+	}
+  
+	connectedCallback() {
+		if (!this.of) {
+			throw new Error('<c-lone> missing `of` property');
+		}
+	}
+  
+	attributeChangedCallback() {
+		this.template = document.getElementById(this.of);
+		this.replaceChildren(this.template.content.cloneNode(true));
+	}
+
+	static get observedAttributes() {
+		return ['of'].concat(super.observedAttributes);
+	}
+}
+
+export { CloneElement };
---
+++ elements/composition/Clone/Clone.test.js
@@ -0,0 +1,15 @@
+import { CloneElement } from './Clone';
+
+window.customElements.define('c-lone', CloneElement);
+
+test('Clones blockquote from template', () => {
+	document.body.innerHTML = `
+    <template id="test">
+      <blockquote>No — Me</blockquote>
+    </template>
+    <c-lone of="test"></c-lone>
+  `;
+	const testElement = document.querySelector('c-lone');
+  const blockquote = testElement.querySelector('blockquote');
+	expect(blockquote).toBeTruthy();
+});
