87a24c2bb0d5ef6d61b7e926af1fade681cea9f3
Heydon <heydon@heydonworks.com>
2023-06-18 13:18:07 +0100

    image element

---
+++ elements/media/Image/Image.js
@@ -0,0 +1,23 @@
+import { ShapeElement } from '../../shape/Shape.js';
+
+/**
+ * @tag i-mage
+ * @summary Placeholder image element, with control over aspect ratio
+ */
+class ImageElement extends ShapeElement {
+	constructor() {
+		super();
+		this.shadowRoot.innerHTML = `<slot></slot>`;
+	}
+
+	render() {
+		const canvas = this.createCanvas(this.aspectRatio);
+		canvas.style.border = '0.125rem solid';
+		let w = this.canvasWidth;
+		let h = this.canvasHeight;
+		canvas.innerHTML = `<path stroke="currentColor" stroke-width="0.125rem" vector-effect="non-scaling-stroke" d="M0,0 ${w},${h} M${w},0 0,${h}"></path>`;
+		this.replaceChildren(canvas);
+	}
+}
+
+export { ImageElement };
---
+++ elements/media/Image/Image.test.js
@@ -0,0 +1,10 @@
+import { ImageElement } from './Image';
+
+window.customElements.define('i-mage', ImageElement);
+
+test('`viewBox` is set to `0 0 200 100`', () => {
+	document.body.innerHTML = '<i-mage aspectratio="2/1"></i-mage>';
+	const svg = document.querySelector('i-mage svg');
+  const viewBox = svg.getAttribute('viewBox');
+	expect(viewBox).toMatch('0 0 200 100');
+});
